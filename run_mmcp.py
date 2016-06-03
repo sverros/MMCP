c3425b4762a2bc01b32a7cbee4a35adf719c1368
Verros <sverros@igskcicgwsgm024.cr.usgs.gov>
2016-06-03 15:48:15 -0600

    removed distance measure

--- run_mmcp.py
+++ run_mmcp.py
@@ -8,27 +8,28 @@ parser.add_argument('direc', metavar='path', type=str,
                    help='path to the inputs directory or directory containing grid, stationlist, uncertainty, and event xml files and fault.txt')
 parser.add_argument('voi', metavar='imt', type=str, 
                    help='string, intensity measures, i.e., pga pgv')
-parser.add_argument('method', metavar='distance_measure', type=str, 
-                   help='string, distance measure, i.e., rjb, rrup')
 parser.add_argument('num_realizations', metavar='N', type = int, 
                    help='integer, number of realizations')
+parser.add_argument('-r', dest='rad', type=int,
+                    default = 45, 
+                    help='radius in km, defaults to 45 km'),
 parser.add_argument('-out', dest='output_directory', type=str,
                     default = 'data_files/', 
                     help='path to output directory, defaults to data_files folder')
+
 args = parser.parse_args()
 direc = args.direc
 num_realizations = args.num_realizations
 voi = args.voi
 voi = voi.split()
-method = args.method
 output_dir = args.output_directory
+radius = args.rad
 
 # Constant variables
-radius = [45]
 # For now the correlation model is constant, as openquake only include this correlation model. 
 corr_model = 'JB2009'
 vscorr = True
 
-run_method(direc, voi, method, num_realizations, radius, corr_model, vscorr, output_dir)
+run_method(direc, voi, num_realizations, radius, corr_model, vscorr, output_dir)
 
  
