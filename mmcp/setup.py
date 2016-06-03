b56c564dbdb904d1af0eeb57209fd96163ef1f41
Verros <sverros@igskcicgwsgm024.cr.usgs.gov>
2016-06-03 14:53:40 -0600

    Added setup

--- mmcp/setup.py
+++ mmcp/setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup
 
-setup(name='mmcp',
+setup(name='MMCP',
       version='0.1dev',
       description='Spatial Correlation Model using Multiple Maps', 
       author='Sarah Verros',
       author_email='sarah.verros@gmail.com',
       url='',
-      packages=['MMCP'],
-      scripts = ['runner.py', 'test.py']
+      scripts = ['runner.py', 'test_mmcp.py']
 )
